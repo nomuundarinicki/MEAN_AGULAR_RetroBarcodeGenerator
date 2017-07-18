357b178d2526ca2c7b9fce10fdd46c696d251778
Nick Chimedbavuu <nickchimedbavuu@Nicks-MacBook-Pro.local>
2017-07-17 21:08:12 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  title: string = 'Retro Barcode Generator';
+  boxes: Array<string> = []
+
+  fillBoxes() {
+    for (let i = 0; i < 10; i++) {
+      const boxColor = (Math.floor(Math.random() * 10))+1;
+      if (boxColor === 1) {
+        this.boxes.push('Pink');
+      } else if (boxColor === 2) {
+        this.boxes.push('Green');
+      } else if (boxColor === 3) {
+        this.boxes.push('Brown');
+      } else if (boxColor === 4) {
+        this.boxes.push('SkyBlue');
+      } else if (boxColor === 5) {
+        this.boxes.push('Orange');
+      } else if (boxColor === 6) {
+        this.boxes.push('Blue');
+      } else if (boxColor === 7) {
+        this.boxes.push('Black');
+      } else if (boxColor === 8) {
+        this.boxes.push('Green');
+      } else if (boxColor === 9) {
+        this.boxes.push('Yellow');
+      } else if (boxColor === 10) {
+        this.boxes.push('Teal');
+      }
+    }
+  }
+
+  ngOnInit() {
+    this.fillBoxes();
+  }
+
 }
